97c5e77d438e447ad80928496cf874d04df08845
Kharel Pawan <kharelpawan1@gmail.com>
2026-08-20 10:24:53 +0300

    fixed: I used the import math and solve the problem

--- Moodle2/tehtävä2.py
+++ Moodle2/tehtävä2.py
@@ -1,7 +1,8 @@
 # 2 Write a program that asks the user for the radius of a circle and the prints out the area of the circle.
+import math
 
 radius = int(input("Enter the radius of the circle: "))
 
-area = 3.14 * radius ** 2
+area = math.pi * radius ** 2
 
-print(area)
+print(f"{area:.2f}")
