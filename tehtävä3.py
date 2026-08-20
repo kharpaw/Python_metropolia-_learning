ed337de82677b7638d11bfbfe7cd479badd1a0b0
Kharel Pawan <kharelpawan1@gmail.com>
2026-08-20 08:46:36 +0300

    fix: correct rectangle perimeter calculation

--- tehtävä3.py
+++ tehtävä3.py
@@ -3,8 +3,7 @@
 length = int(input("Enter the length of rectangle: "))
 width = int(input("Enter the width of the rectangle: "))
 
-
-perimeter = 2 * (length + 2 * width)
+perimeter = 2 * (length + width)
 area = length * width
 
 print(perimeter)
