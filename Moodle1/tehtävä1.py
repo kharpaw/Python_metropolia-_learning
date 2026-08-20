a8598bfb338d6dcbed7b6e6743f6ada98f3082ab
Kharel Pawan <kharelpawan1@gmail.com>
2026-08-20 09:16:28 +0300

    add: I added new file to the moodle1

---
+++ Moodle1/tehtävä1.py
@@ -0,0 +1,8 @@
+# 1 Write a program that asks your name and then greets you by your name:
+
+hello = input("What is you Name: ")
+greet = "Nice to meet you"
+
+print(f"Hi {hello}, {greet}")
+
+
---
+++ Moodle1/tehtävä2.py
@@ -0,0 +1,3 @@
+hello = input("Enter your name: ")
+
+print(f"Hi, {hello}")
